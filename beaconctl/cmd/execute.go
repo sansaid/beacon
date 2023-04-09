3a29920561574f6ef9e919d8001656a706948353
Sanyia Saidova <sanyia.saidova@gmail.com>
2023-04-09 21:59:21 +0000

    Started implementing interface for beacond client

--- beaconctl/cmd/execute.go
+++ beaconctl/cmd/execute.go
@@ -4,34 +4,50 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var RESOURCES = []string{"probe"}
+
 var beaconctl = &cobra.Command{
 	Use:   "beaconctl",
 	Short: "beaconctl is your CLI based controller for beacond",
 	Args:  cobra.MinimumNArgs(1),
 }
 
-var probe = &cobra.Command{
-	Use:   "probe",
-	Short: "manage your probes",
-	Args:  cobra.MinimumNArgs(1),
+var crudCmds = []*cobra.Command{
+	{
+		Use:       "create",
+		Short:     "create a resource",
+		ValidArgs: RESOURCES,
+	},
+	{
+		Use:       "delete",
+		Short:     "delete a resource",
+		ValidArgs: RESOURCES,
+	},
+	{
+		Use:       "list",
+		Short:     "list a resource",
+		ValidArgs: RESOURCES,
+	},
+	{
+		Use:       "describe",
+		Short:     "describe a resource",
+		ValidArgs: append(RESOURCES, "beacon"),
+	},
 }
-
-var probeAdd = &cobra.Command{
-	Use:   "add",
-	Short: "add a resource",
-}
-
-var flagProbe string
 
 func init() {
 	// TODO: create a beacond client for beaconctl to communicate with
-	probeAdd.PersistentFlags().VarP(&flagProbe, "")
-	probe.AddCommand(probeAdd)
-	beaconctl.AddCommand(probe)
+	initialiseCrudCmds()
 }
 
 func beaconctlHndlr(cmd *cobra.Command, args []string) {
 	panic("not implemented")
+}
+
+func initialiseCrudCmds() {
+	for _, cmd := range crudCmds {
+		beaconctl.AddCommand(cmd)
+	}
 }
 
 func Execute() error {
